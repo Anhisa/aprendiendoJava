b78d1a1496b9b75fce816e946d5908496d441953
Anhisa <anhisa@hotmail.com>
2021-07-12 10:46:55 -0500

    adicion de funciones time al reloj

--- reloj/codigojava.js
+++ reloj/codigojava.js
@@ -15,3 +15,13 @@ const actualizarHora = ()=>{
 }
 actualizarHora();
 setInterval(actualizarHora, 1000);
+
+let fecha = new Date(2070,11,28);
+
+console.log(fecha.getYear()); 
+// getyear mmuestra el año menos 1900,funcion: console.log(fecha.getYear()+1900);
+console.log(fecha.getFullYear());
+console.log(fecha.getMonth());
+// getMonth nos devuelve el número de mes en forma de array, entonces, Enero = 0 y Diciembre = 11
+console.log(fecha.getDay());
+// getDay nos devuelve el número de día en forma de array, entonces, Domingo = 0 y Sábado = 6
